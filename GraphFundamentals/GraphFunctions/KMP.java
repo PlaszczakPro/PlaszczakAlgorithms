1baa55d25673fbfd0e2f321fe0e6ebb5747ea4e9
Adam Dybcio <adam.dybcio.kontakt@gmail.com>
2024-06-16 22:11:01 +0200

    Updated KMP

--- GraphFundamentals/GraphFunctions/KMP.java
+++ GraphFundamentals/GraphFunctions/KMP.java
@@ -9,69 +9,56 @@ import java.util.Scanner;
 public class KMP {
     public static int[] computePrefixFunction(String pattern) {
         int[] prefixFunction = new int[pattern.length()];
+        int lenght=0;
+        int i=1;
         prefixFunction[0] = 0;
         int k = 0;
-        for (int q = 1; q < pattern.length(); q++) {
-            while (k > 0 && pattern.charAt(k) != pattern.charAt(q)) {
-                k = prefixFunction[k - 1];
+        while(i<pattern.length()){
+            if(pattern.charAt(i)==pattern.charAt(lenght)){
+                lenght++;
+                prefixFunction[i]=lenght;
+                i++;
+            }else{
+                if(lenght!=0){
+                    lenght=prefixFunction[lenght-1];
+                }else{
+                    prefixFunction[i]=lenght;
+                    i++;
+                }
             }
-            if (pattern.charAt(k) == pattern.charAt(q)) {
-                k++;
-            }
-            prefixFunction[q] = k;
         }
         return prefixFunction;
     }
-
-    public static int kmpMatcher(File file, String pattern) throws FileNotFoundException {
-
-        StringBuilder text = new StringBuilder();
-
-        Scanner scanner = new Scanner(file);
-        while (scanner.hasNextLine()){
-            text.append(scanner.nextLine());
-            text.append("\n");
-        }
-        int n = text.length();
-        int m = pattern.length();
-        int[] prefixFunction = computePrefixFunction(pattern);
-        int q = 0;
-        for (int i = 0; i < n; i++) {
-            while (q > 0 && pattern.charAt(q) != text.charAt(i)) {
-                q = prefixFunction[q - 1];
-            }
-            if (pattern.charAt(q) == text.charAt(i)) {
-                q++;
-            }
-            if (q == m) {
-                return i - m + 1;
-            }
-        }
-        return -1;
-    }
     public static void kmpReplace(File file, String pattern, String replacement) throws IOException {
-
         StringBuilder text = new StringBuilder();
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()){
             text.append(scanner.nextLine());
             text.append("\n");
         }
-        int n = text.length();
-        int m = pattern.length();
         int[] prefixFunction = computePrefixFunction(pattern);
-        int q = 0;
-        for (int i = 0; i < n; i++) {
-            while (q > 0 && pattern.charAt(q) != text.charAt(i)) {
-                q = prefixFunction[q - 1];
+        int m=pattern.length();
+        int n=text.length();
+        int j=0;
+        int i=0;
+        while ((n-i)>=(m-j)){
+            if(pattern.charAt(j)==text.charAt(i)){
+                i++;
+                j++;
             }
-            if (pattern.charAt(q) == text.charAt(i)) {
-                q++;
+            if(j==m){
+                text.replace(i-j,i,replacement);
+                n=text.length();
+                i=i-m+j+replacement.length();
+                j=prefixFunction[j-1];
             }
-            if (q == m) {
-                text.replace(i - m + 1, i + 1, replacement);
-                q = 0;
-                n = text.length();
+            else if(i<n && pattern.charAt(j)!=text.charAt(i)){
+                if(j!=0){
+                    j=prefixFunction[j-1];
+                }
+                else{
+                    i++;
+                }
             }
         }
         BufferedWriter writer= new BufferedWriter(new FileWriter(file));
